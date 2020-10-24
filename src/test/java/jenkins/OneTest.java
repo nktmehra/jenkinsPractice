1af7ad3139658e8c23b319ef1285efc53bde1f68
Ankit <nktmehra2002@gmail.com>
2020-10-24 19:21:27 +0530

    Migrated to TestNg from Junit and updated the Jenkins File

--- src/test/java/jenkins/OneTest.java
+++ src/test/java/jenkins/OneTest.java
@@ -1,11 +1,32 @@
 package jenkins;
 
-import org.junit.Test;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class OneTest {
-	
+
+	@BeforeMethod
+	public void beforeMethod() {
+		long id = Thread.currentThread().getId();
+		System.out.println("Before test-method. Thread id is: " + id);
+	}
+
 	@Test
-	public void test() {
-		System.out.println("Hello World");
+	public void testMethodsOne() {
+		long id = Thread.currentThread().getId();
+		System.out.println("Simple test-method One. Thread id is: " + id);
+	}
+
+	@Test
+	public void testMethodsTwo() {
+		long id = Thread.currentThread().getId();
+		System.out.println("Simple test-method Two. Thread id is: " + id);
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		long id = Thread.currentThread().getId();
+		System.out.println("After test-method. Thread id is: " + id);
 	}
 }
